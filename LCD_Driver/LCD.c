cf1fbf45262e30fa2fe5b5d06d00e5af142b44e1
MarkFayez95 <markfayeztalaat@gmail.com>
2021-03-30 21:12:19 +0200

    LCD Driver v0.6.0

--- LCD_Driver/LCD.c
+++ LCD_Driver/LCD.c
@@ -6,30 +6,144 @@
  */ 
 #include "LCD.h"
 
-
 void LCD_Init (void)
 {
+	#if LCD_MODE == 4
 	
+	DIO_SetPinDir(LCD_4BIT_MODE_CMD_PORT, LCD_RS_PIN , DIO_PIN_OUTPUT);
+	DIO_SetPinDir(LCD_4BIT_MODE_CMD_PORT, LCD_RW_PIN , DIO_PIN_OUTPUT);
+	DIO_SetPinDir(LCD_4BIT_MODE_CMD_PORT, LCD_E_PIN , DIO_PIN_OUTPUT);
+	
+	DIO_SetPinDir(LCD_4BIT_MODE_DATA_PORT, LCD_D4_PIN , DIO_PORT_OUTPUT);
+	DIO_SetPinDir(LCD_4BIT_MODE_DATA_PORT, LCD_D5_PIN , DIO_PORT_OUTPUT);
+	DIO_SetPinDir(LCD_4BIT_MODE_DATA_PORT, LCD_D6_PIN , DIO_PORT_OUTPUT);
+	DIO_SetPinDir(LCD_4BIT_MODE_DATA_PORT, LCD_D7_PIN , DIO_PORT_OUTPUT);
+	
+	_delay_ms(100);
+	
+	LCD_WriteCommand(0x33);
+	LCD_WriteCommand(0x32);
+	LCD_WriteCommand(0x28);
+	
+	LCD_WriteCommand(0x0C);
+	LCD_WriteCommand(0x01);
+	LCD_WriteCommand(0x06);
+	LCD_WriteCommand(0x02);
+	
+	#elif LCD_MODE == 8
+	
+	DIO_SetPinDir(LCD_8BIT_MODE_CMD_PORT, LCD_RS_PIN , DIO_PIN_OUTPUT);
+	DIO_SetPinDir(LCD_8BIT_MODE_CMD_PORT, LCD_RW_PIN , DIO_PIN_OUTPUT);
+	DIO_SetPinDir(LCD_8BIT_MODE_CMD_PORT, LCD_E_PIN , DIO_PIN_OUTPUT);
+	
+	DIO_SetPortDir(LCD_8BIT_MODE_DATA_PORT, DIO_PORT_OUTPUT);
+	
+	_delay_ms(100);
+	
+	LCD_WriteCommand(0x38);
+	
+	LCD_WriteCommand(0x0C);
+	LCD_WriteCommand(0x01);
+	LCD_WriteCommand(0x06);
+	LCD_WriteCommand(0x02);
+	
+	#endif
 }
 
 void LCD_WriteCommand (uint8 cmd)
 {
 	
+	#if LCD_MODE == 4
+	
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_RS_PIN, DIO_PIN_LOW);
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_RW_PIN, DIO_PIN_LOW);
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_LOW);
+	
+	LCD_4BIT_MODE_DATA_PORT |= (cmd & 0xF0);
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_HIGH);
+	_delay_ms(1);
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_LOW);
+	
+	LCD_4BIT_MODE_DATA_PORT |= (cmd << 4);
+	
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_HIGH);
+	_delay_ms(1);
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_LOW);
+	
+	_delay_ms(5);
+	
+	#elif LCD_MODE == 8
+	
+	DIO_SetPinValue(LCD_8BIT_MODE_CMD_PORT. LCD_RS_PIN, DIO_PIN_LOW);
+	DIO_SetPinValue(LCD_8BIT_MODE_CMD_PORT. LCD_RW_PIN, DIO_PIN_LOW);
+	DIO_SetPinValue(LCD_8BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_LOW);
+	
+	LCD_8BIT_MODE_DATA_PORT = cmd;
+	DIO_SetPinValue(LCD_8BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_HIGH);
+	_delay_ms(1);
+	DIO_SetPinValue(LCD_8BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_LOW);
+	
+	_delay_ms(5);
+	
+	#endif
 }
 
 void LCD_WriteChar (uint8 data)
 {
+	#if LCD_MODE == 4
+	
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_RS_PIN, DIO_PIN_HIGH);
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_RW_PIN, DIO_PIN_LOW);
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_LOW);
+	
+	LCD_4BIT_MODE_DATA_PORT |= (data & 0xF0);
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_HIGH);
+	_delay_ms(1);
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_LOW);
+	
+	LCD_4BIT_MODE_DATA_PORT |= (data << 4);
+	
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_HIGH);
+	_delay_ms(1);
+	DIO_SetPinValue(LCD_4BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_LOW);
+	
+	_delay_ms(5);
+	
+	#elif LCD_MODE == 8
+	
+	DIO_SetPinValue(LCD_8BIT_MODE_CMD_PORT. LCD_RS_PIN, DIO_PIN_HIGH);
+	DIO_SetPinValue(LCD_8BIT_MODE_CMD_PORT. LCD_RW_PIN, DIO_PIN_LOW);
+	DIO_SetPinValue(LCD_8BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_LOW);
+	
+	LCD_8BIT_MODE_DATA_PORT = data;
+	DIO_SetPinValue(LCD_8BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_HIGH);
+	_delay_ms(1);
+	DIO_SetPinValue(LCD_8BIT_MODE_CMD_PORT. LCD_E_PIN, DIO_PIN_LOW);
+	
+	_delay_ms(5);
+	
+	#endif
+	
+	LCD_WriteCommand (0x06);
 	
 }
 
+
+/*  Assignment  */
 void LCD_WriteInteger (sint32 num)
 {
 	
 }
+/******************/
 
 void LCD_WriteString(uint8* str)
 {
-	
+	uint8 Str_count = 0;
+	while (str[Str_count] != '\0')
+	{
+		LCD_WriteChar (str[Str_count]);
+		Str_count++;
+	}
 }
 
 void LCD_GoToLocation(uint8 row, uint8 col)
@@ -39,6 +153,6 @@ void LCD_GoToLocation(uint8 row, uint8 col)
 
 void LCD_Clear (void)
 {
-	
+	LCD_WriteCommand(0x01);
 }
 
--- LCD_Driver/LCD_CFG.h
+++ LCD_Driver/LCD_CFG.h
@@ -11,35 +11,35 @@
 
 #include "DIO.h"
 
-#define	LCD_Mode	4
+#define	LCD_MODE	4
 
-#if LCD_Mode == 4
+#if LCD_MODE == 4
 
-	#define		LCD_4BIT_MODE_CMD_PORT		DIO_PORTB
-	#define		LCD_4BIT_MODE_DATA_PORT		DIO_PORTA
+#define		LCD_4BIT_MODE_CMD_PORT		DIO_PORTB
+#define		LCD_4BIT_MODE_DATA_PORT		DIO_PORTA
 
-	#define		LCD_RS_PIN		DIO_PIN_1
-	#define		LCD_RW_PIN		DIO_PIN_2
-	#define		LCD_E_PIN		DIO_PIN_3
+#define		LCD_RS_PIN		DIO_PIN_1
+#define		LCD_RW_PIN		DIO_PIN_2
+#define		LCD_E_PIN		DIO_PIN_3
 
-#elif LCD_Mode == 8
+#elif LCD_MODE == 8
 
-	#define		LCD_8BIT_MODE_CMD_PORT		DIO_PORTA
-	#define		LCD_8BIT_MODE_DATA_PORT		DIO_PORTB
+#define		LCD_8BIT_MODE_CMD_PORT		DIO_PORTA
+#define		LCD_8BIT_MODE_DATA_PORT		DIO_PORTB
 
-	#define		LCD_RS_PIN		DIO_PIN_1
-	#define		LCD_RW_PIN		DIO_PIN_2
-	#define		LCD_E_PIN		DIO_PIN_3
+#define		LCD_RS_PIN		DIO_PIN_1
+#define		LCD_RW_PIN		DIO_PIN_2
+#define		LCD_E_PIN		DIO_PIN_3
 
 #endif
 
-#define		LCD_D0		DIO_PIN_0
-#define		LCD_D1		DIO_PIN_1
-#define		LCD_D2		DIO_PIN_2
-#define		LCD_D3		DIO_PIN_3
-#define		LCD_D4		DIO_PIN_4
-#define		LCD_D5		DIO_PIN_5
-#define		LCD_D6		DIO_PIN_6
-#define		LCD_D7		DIO_PIN_7
+#define		LCD_D0_PIN		DIO_PIN_0
+#define		LCD_D1_PIN		DIO_PIN_1
+#define		LCD_D2_PIN		DIO_PIN_2
+#define		LCD_D3_PIN		DIO_PIN_3
+#define		LCD_D4_PIN		DIO_PIN_4
+#define		LCD_D5_PIN		DIO_PIN_5
+#define		LCD_D6_PIN		DIO_PIN_6
+#define		LCD_D7_PIN		DIO_PIN_7
 
 #endif /* LCD_CFG_H_ */
